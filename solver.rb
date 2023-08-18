b78cd216411e4f9eb8db3eacfe12a92ea116f6a1
Derrick1451 <haylederrick14@gmail.com>
2023-08-18 12:28:34 +0300

    Edit required changes

--- solver.rb
+++ solver.rb
@@ -1,35 +1,31 @@
 class Solver
-    def factorial(num)
-      raise 'Factorial is not defined for negative numbers' if num.negative?
-  
-      result = 1
-      (1..num).each do |i|
-        result *= i
-      end
-      result
-    end
-  
-    def fizzbuzz(number)
-      result = []
-  
-      (1..number).each do |i|
-        if (i % 3).zero? && (i % 5).zero?
-          result << 'FizzBuzz'
-        elsif (i % 3).zero?
-          result << 'Fizz'
-        elsif (i % 5).zero?
-          result << 'Buzz'
-        else
-          result << i
-        end
-      end
-  
-      result
-    end
-  
-    def reverse(str)
-      str.reverse
-    end
-end
+  def factorial(num)
+    raise 'Factorial is not defined for negative numbers' if num.negative?
 
-  
+    result = 1
+    (1..num).each do |i|
+      result *= i
+    end
+    result
+  end
+
+  def fizzbuzz(number)
+    result = []
+
+    (1..number).each do |i|
+      result << if (i % 3).zero? && (i % 5).zero?
+                  'FizzBuzz'
+                elsif (i % 3).zero?
+                  'Fizz'
+                elsif (i % 5).zero?
+                  'Buzz'
+                else
+                  i
+                end
+    end
+  end
+
+  def reverse(str)
+    str.reverse
+  end
+end
